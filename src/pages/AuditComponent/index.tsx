fbf2a45079908098f3693b045dc3bc70fc5cc693
Sanjaybharathi-hyrrokkin <sanjay.hyrrokkin@gmail.com>
2022-01-04 15:57:48 +0530

    sanjay - audit, roles, groups

--- src/pages/AuditComponent/index.tsx
+++ src/pages/AuditComponent/index.tsx
@@ -104,6 +104,194 @@ const AuditComponent = () => {
               </li>
             </ol>
           </nav>
+          <div className="row audit">
+            <div className="col-12">
+              <form className="d-flex filter_form">
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Filters by users"
+                    value={FilterByUser}
+                    name="FilterByUser"
+                    onChange={HandleFormChange}
+                  />
+                  <button
+                    className="btn btn-transparent"
+                    type="button"
+                    onClick={() => setFilterUsersPopup(true)}
+                  >
+                    <img src={FilterSvg} alt="filter" className="img-fluid" />
+                  </button>
+                </div>
+              </form>
+
+              <div className="table-responsive">
+                <table id="folders" className="w-100 table">
+                  <thead>
+                    <tr>
+                      <td>User Name</td>
+                      <td>Entity ID</td>
+                      <td>Action</td>
+                      <td>Prev. Date </td>
+                      <td>Pre Value</td>
+                      <td>New Value</td>
+                      <td>Updated Date</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {!!AuditListData && AuditListData.length ? (
+                      AuditListData.map((item, index) => {
+                        return (
+                          <tr key={index}>
+                            <td>
+                              <div className="media">
+                                <span>
+                                  <img
+                                    // src={`../../asset/img/${item.profileImage}`}
+                                    src={StaticProfile}
+                                    alt="profile"
+                                    className="img-fluid"
+                                  />
+                                </span>
+                                <div className="media-body">
+                                  <h6>{item.username}</h6>
+                                  <p>{item.userRole}</p>
+                                </div>
+                              </div>
+                            </td>
+                            <td>
+                              <small className="id">{item.entityId}</small>
+                            </td>
+                            <td>
+                              <span className="text-theme">{item.action}</span>
+                            </td>
+                            <td>
+                              {/* 29 Jun 2020 */}
+                              {item.previousDate}
+                              <br />
+                              <small>21:37</small>
+                            </td>
+                            <td>xxxxxxx</td>
+                            <td>xxxxxxx</td>
+                            {/* <td>07 May 2020</td> */}
+                            <td>{item.updatedDate}</td>
+                          </tr>
+                        );
+                      })
+                    ) : (
+                      <></>
+                    )}
+                  </tbody>
+                </table>
+              </div>
+
+              <ReactPaginate
+                previousLabel={"<"}
+                nextLabel={">"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                pageCount={6}
+                marginPagesDisplayed={2}
+                disableInitialCallback={true}
+                initialPage={1}
+                pageRangeDisplayed={2}
+                disabledClassName={"disable-page"}
+                // onPageChange={HandlePageClick}
+                containerClassName={"pagination-styles"}
+                // subContainerClassName={`pages pagination`}
+                activeClassName={"active"}
+              />
+
+              {!!FilterUsersPopup ? (
+                <CommonPopup
+                  ModelClassName={"modal_user"}
+                  OnClose={HandleCloseFilterPopup}
+                >
+                  <form className="row mt-4">
+                    <div className="col-12">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                        value={AuditFilterState.Name}
+                        name="Name"
+                        onChange={HandleFilterStateChange}
+                      />
+                    </div>
+                    <div className="col-12">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Organization Search"
+                        value={AuditFilterState.Organization}
+                        name="Organization"
+                        onChange={HandleFilterStateChange}
+                      />
+                    </div>
+                    <div className="col-12 col-md-6">
+                      <input
+                        // type="date"
+                        // data-toggle="datepicker"
+                        className="form-control"
+                        placeholder="Start Date"
+                        type="text"
+                        onFocus={(e) => {
+                          e.currentTarget.type = "date";
+                          e.currentTarget.focus();
+                        }}
+                        onBlur={(e) => {
+                          e.currentTarget.type = "text";
+                        }}
+                        min={new Date().toISOString().split("T")[0]}
+                        value={AuditFilterState.StartDate}
+                        name="StartDate"
+                        onChange={HandleFilterStateChange}
+                      />
+                    </div>
+                    <div className="col-12 col-md-6">
+                      <input
+                        className="form-control"
+                        placeholder="End Date"
+                        // this is for set the type of input on focus of the field
+                        type="text"
+                        onFocus={(e) => {
+                          e.currentTarget.type = "date";
+                          e.currentTarget.focus();
+                        }}
+                        // this is for set the type of input on outside click of the field
+                        onBlur={(e) => {
+                          e.currentTarget.type = "text";
+                        }}
+                        min={new Date().toISOString().split("T")[0]}
+                        value={AuditFilterState.EndDate}
+                        name="EndDate"
+                        onChange={HandleFilterStateChange}
+                      />
+                    </div>
+                    <div className="col-12 d-flex mt-2">
+                      <button
+                        type="button"
+                        className="btn btn-submit"
+                        onClick={HandleCloseFilterPopup}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-theme ms-3"
+                        onClick={HandleFilterSearch}
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                </CommonPopup>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
         </section>
       </div>
     </>
--- src/pages/GroupComponent/index.tsx
+++ src/pages/GroupComponent/index.tsx
@@ -4,6 +4,81 @@ import UserIconsSvg from "../../asset/img/users-icons.svg";
 import CommonPopup from "../../Components/CommonPopup";
 import GroupConfigData from "../../Config/GroupConfig.json";
 
+import ReactMultiSelectCheckboxes from "react-multiselect-checkboxes";
+
+// const ReactMultiSelectCheckboxes = require("react-multiselect-checkboxes");
+
+let UserArray = [
+  {
+    id: 1,
+    value: "9ce42304-b732-4791-9731-6f299b6df8c7",
+    label: "User 1",
+  },
+  {
+    id: 2,
+    value: "90419f06-7d07-45c8-bcec-d675096fe27f",
+    label: "User 2",
+  },
+  {
+    id: 3,
+    value: "a23521da-0a48-4ef6-baa2-d727704f65c2",
+    label: "User 3",
+  },
+  {
+    id: 4,
+    value: "34b2b58a-0123-49e2-b2de-1eef0922139b",
+    label: "User 4",
+  },
+  {
+    id: 5,
+    value: "0fceaec7-fb46-4aef-994b-863a97130168",
+    label: "User 5",
+  },
+  {
+    id: 6,
+    value: "b68c6b76-bbe0-49a6-8a90-209b8d88d353",
+    label: "User 6",
+  },
+];
+
+let UserArrayLatest = [
+  {
+    groupSlug: 1,
+    userSlug: "9ce42304-b732-4791-9731-6f299b6df8c7",
+    userName: "User 1",
+    userEmail: "User.1@gmail.com",
+  },
+  {
+    groupSlug: 2,
+    userSlug: "90419f06-7d07-45c8-bcec-d675096fe27f",
+    userName: "User 2",
+    userEmail: "User.2@gmail.com",
+  },
+  {
+    groupSlug: 3,
+    userSlug: "a23521da-0a48-4ef6-baa2-d727704f65c2",
+    userName: "User 3",
+    userEmail: "User.3@gmail.com",
+  },
+  {
+    groupSlug: 4,
+    userSlug: "34b2b58a-0123-49e2-b2de-1eef0922139b",
+    userName: "User 4",
+    userEmail: "User.4@gmail.com",
+  },
+  {
+    groupSlug: 5,
+    userSlug: "0fceaec7-fb46-4aef-994b-863a97130168",
+    userName: "User 5",
+    userEmail: "User.5@gmail.com",
+  },
+  {
+    groupSlug: 6,
+    userSlug: "b68c6b76-bbe0-49a6-8a90-209b8d88d353",
+    userName: "User 6",
+    userEmail: "User.6@gmail.com",
+  },
+];
 // object structure for roles item
 interface GroupListItem {
   groupName: string;
@@ -11,6 +86,17 @@ interface GroupListItem {
   groupSlug: string;
   orgSlug: string;
   profileImage: string;
+}
+interface UserListItem {
+  id: number;
+  value: string;
+  label: string;
+}
+interface UserListItemLatest {
+  userSlug: string;
+  userName: string;
+  userEmail: string;
+  groupSlug: string;
 }
 
 const GroupComponent = () => {
@@ -27,7 +113,19 @@ const GroupComponent = () => {
   const [EditGroupPopup, setEditGroupPopup] = useState(false);
 
   // this is for storing seleted data
-  const [SelectedGroupItem, setSelectedGroupItem] = useState({});
+  const [SelectedGroupItem, setSelectedGroupItem] = useState(
+    {} as GroupListItem
+  );
+
+  // add user popup handling
+  const [AddUserPopup, setAddUserPopup] = useState(false);
+
+  // add search user popup handling
+  const [SearchUserPopup, setSearchUserPopup] = useState(false);
+
+  const [SelectedUserList, setSelectedUserList] = useState(
+    [] as UserListItem[]
+  );
 
   useEffect(() => {
     // ... your initial api call inside here
@@ -75,10 +173,357 @@ const GroupComponent = () => {
     setEditGroupPopup(true);
   };
 
+  const OnClickAddUser = (group: any) => {
+    setSelectedGroupItem(group);
+    setAddUserPopup(true);
+  };
+
+  const OnClickSearchUser = (group: any) => {
+    setSelectedGroupItem(group);
+    setSearchUserPopup(true);
+  };
+
+  // handling popup close when outside click
+  const HandleDropdownBlur = (event: any) => {
+    if (!event.currentTarget.contains(event.relatedTarget)) {
+      setTimeout(() => {
+        !!AddUserPopup && setAddUserPopup(false);
+        !!SearchUserPopup && setSearchUserPopup(false);
+      }, 500);
+    }
+  };
+
+  const OnChangeUserSelection = (value) => {
+    setSelectedUserList(value);
+  };
+
   return (
     <>
       <div className="page_header">
         <h1>Groups</h1>
+      </div>
+      <div className="container">
+        <section className="administrator">
+          <div className="row organizations">
+            <div className="col-12">
+              <div className="text-end">
+                <button
+                  type="button"
+                  className="btn-add"
+                  data-bs-toggle="modal"
+                  data-bs-target="#new_role"
+                  onClick={() => setCreateGroupPopup(true)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 57 57"
+                    id="add"
+                  >
+                    <g id="Group_212" transform="translate(-1812 -423)">
+                      <g id="Group_95" transform="translate(75 94)">
+                        <g
+                          id="Ellipse_41"
+                          transform="translate(1737 329)"
+                          fill="#f3f3f3"
+                          stroke="currentColor"
+                          stroke-width="2"
+                        >
+                          <circle
+                            cx="28.5"
+                            cy="28.5"
+                            r="28.5"
+                            stroke="none"
+                          ></circle>
+                          <circle
+                            cx="28.5"
+                            cy="28.5"
+                            r="27.5"
+                            fill="none"
+                          ></circle>
+                        </g>
+                        <g
+                          id="Group_94"
+                          transform="translate(1752.967 344.966)"
+                        >
+                          <rect
+                            id="Rectangle_258"
+                            width="25.067"
+                            height="3.76"
+                            rx="1.88"
+                            transform="translate(0 10.653)"
+                            fill="currentColor"
+                          ></rect>
+                          <rect
+                            id="Rectangle_259"
+                            width="25.067"
+                            height="3.76"
+                            rx="1.88"
+                            transform="translate(14.413 0) rotate(90)"
+                            fill="currentColor"
+                          ></rect>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </button>
+              </div>
+              <div className="col-12 my-4">
+                <div className="row">
+                  {!!GroupList && GroupList.length ? (
+                    GroupList.map((group) => {
+                      return (
+                        <div className="col-12 col-md-6 col-lg-4">
+                          <div className="card">
+                            <span
+                              className="action"
+                              onClick={() => OnClickEditGroup(group)}
+                            >
+                              <svg className="action_icon" width="15">
+                                <use xlinkHref={UserIconsSvg + "#edit"}></use>
+                              </svg>
+                            </span>
+                            <div className="body">
+                              <img
+                                src={group.profileImage}
+                                alt="profile"
+                                className="profile_picture img-fluid mb-3"
+                              />
+                              <h5>{group.groupName}</h5>
+                              <p>{group.settingsName}</p>
+                            </div>
+                            <div className="footer">
+                              <span
+                                className="action add-user-parent"
+                                tabIndex={1}
+                                onClick={() => OnClickAddUser(group)}
+                                onBlur={(event) => HandleDropdownBlur(event)}
+                              >
+                                <svg className="action_icon">
+                                  <use
+                                    xlinkHref={UserIconsSvg + "#new-user"}
+                                  ></use>
+                                </svg>
+                                {!!AddUserPopup &&
+                                SelectedGroupItem.groupName ===
+                                  group.groupName ? (
+                                  <div
+                                    className="add-user-container"
+                                    onClick={(event) => {
+                                      event.stopPropagation();
+                                    }}
+                                  >
+                                    <div className="w-100 p-4">
+                                      <input
+                                        type={"text"}
+                                        className="search-text-input"
+                                        onClick={(event) =>
+                                          event.preventDefault()
+                                        }
+                                      />
+                                    </div>
+                                    <div className="user-list-contianer">
+                                      <div className="user-item">
+                                        <span>User 1</span>
+                                        <span>
+                                          <Feather.XCircle className="close-svg" />
+                                        </span>
+                                      </div>
+                                      <div className="user-item">
+                                        <span>User 1</span>
+                                        <span>
+                                          <Feather.XCircle className="close-svg" />
+                                        </span>
+                                      </div>
+                                      <div className="user-item">
+                                        <span>User 1</span>
+                                        <span>
+                                          <Feather.XCircle className="close-svg" />
+                                        </span>
+                                      </div>
+                                    </div>
+                                    <button
+                                      type="button"
+                                      className="btn btn-theme ms-auto"
+                                    >
+                                      Ok
+                                    </button>
+                                  </div>
+                                ) : (
+                                  <></>
+                                )}
+                              </span>
+                              <span
+                                className="action search-user-parent"
+                                tabIndex={1}
+                                onClick={() => OnClickSearchUser(group)}
+                                onBlur={(event) => HandleDropdownBlur(event)}
+                              >
+                                <svg className="action_icon">
+                                  <use
+                                    xlinkHref={UserIconsSvg + "#group"}
+                                  ></use>
+                                </svg>
+                                {!!SearchUserPopup &&
+                                SelectedGroupItem.groupName ===
+                                  group.groupName ? (
+                                  <div
+                                    className="search-user-container"
+                                    onClick={(event) => {
+                                      event.stopPropagation();
+                                    }}
+                                  >
+                                    <div className="w-100 p-4">
+                                      {/* <input
+                                        type={"text"}
+                                        className="search-text-input"
+                                        onClick={(event) =>
+                                          event.preventDefault()
+                                        }
+                                      /> */}
+                                      <ReactMultiSelectCheckboxes
+                                        options={UserArray}
+                                        onChange={OnChangeUserSelection}
+                                      />
+                                    </div>
+                                    <div className="user-list-contianer">
+                                      {!!SelectedUserList &&
+                                      SelectedUserList.length ? (
+                                        SelectedUserList.map((user) => {
+                                          return (
+                                            <div
+                                              key={user.value}
+                                              className="user-item"
+                                            >
+                                              <span>{user.label}</span>
+                                              <span>
+                                                <Feather.XCircle className="close-svg" />
+                                              </span>
+                                            </div>
+                                          );
+                                        })
+                                      ) : (
+                                        <></>
+                                      )}
+                                      {/* <div className="user-item">
+                                        <span>Selected User 1</span>
+                                        <span>
+                                          <Feather.XCircle className="close-svg" />
+                                        </span>
+                                      </div>
+                                      <div className="user-item">
+                                        <span>Selected User 1</span>
+                                        <span>
+                                          <Feather.XCircle className="close-svg" />
+                                        </span>
+                                      </div>
+                                      <div className="user-item">
+                                        <span>Selected User 1</span>
+                                        <span>
+                                          <Feather.XCircle className="close-svg" />
+                                        </span>
+                                      </div> */}
+                                    </div>
+                                    <button
+                                      type="button"
+                                      className="btn btn-theme ms-auto ok-button"
+                                    >
+                                      Ok
+                                    </button>
+                                  </div>
+                                ) : (
+                                  <></>
+                                )}
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {!!CreateGroupPopup ? (
+          <CommonPopup
+            ModelClassName={"modal_user"}
+            Heading="Create Group Name"
+            OnClose={HandleCloseCreatePopup}
+          >
+            <form className="row mt-4">
+              <div className="col-12">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Group Name"
+                  value={NewGroupName}
+                  name="NewGroupName"
+                  onChange={HandleFormChange}
+                />
+              </div>
+              <div className="col-12 d-flex mt-2">
+                <button
+                  type="button"
+                  className="btn btn-submit"
+                  onClick={HandleCloseCreatePopup}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-theme ms-3"
+                  onClick={HandleCreateGropup}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </CommonPopup>
+        ) : (
+          <></>
+        )}
+
+        {!!EditGroupPopup ? (
+          <CommonPopup
+            ModelClassName={"modal_user"}
+            Heading="Edit Group Name"
+            OnClose={HandleCloseEditPopup}
+          >
+            <form className="row mt-4">
+              <div className="col-12">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Group Name"
+                  value={NewGroupName}
+                  name="NewGroupName"
+                  onChange={HandleFormChange}
+                />
+              </div>
+              <div className="col-12 d-flex mt-2">
+                <button
+                  type="button"
+                  className="btn btn-submit"
+                  onClick={HandleCloseEditPopup}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-theme ms-3"
+                  onClick={HandleEditGropup}
+                >
+                  Update
+                </button>
+              </div>
+            </form>
+          </CommonPopup>
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
